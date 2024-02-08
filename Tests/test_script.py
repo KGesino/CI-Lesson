3ffb0e574b315c7f1ca0e0b3be0c7e5702b24055
KGesino <kgesino@gmail.com>
2024-02-07 21:07:02 -0700

    hello

---
+++ Tests/test_script.py
@@ -0,0 +1,10 @@
+import subprocess
+import sys
+
+def test_script_output(capfd):
+    # Run the script file under a new process using the same python interpreter
+    subprocess.run([sys.executable, './script.py'])
+    # Capture the process output
+    captured = capfd.readouterr()
+    # Assert that the output is correct
+    assert captured.out == "Hello, World\r\n"
